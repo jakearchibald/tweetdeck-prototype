a027251a10ba2f8966b111fba358ee2ef3886621
Jake Archibald <jaffathecake@gmail.com>
2014-08-04 17:40:03 +0100

    Simplicity-first

--- www/static/js/lib/domtoreact.js
+++ www/static/js/lib/domtoreact.js
@@ -1,25 +1,28 @@
 var DOM = require('react').DOM;
-
+ 
 module.exports = function domToReact(el) {
-  if (el.nodeType == 1) {
-    var funcName = el.nodeName.toLowerCase();
-    var props = {};
-
-    Array.prototype.forEach.call(el.attributes, function(attribute) {
-      var name = attribute.nodeName;
-      if (name == 'class') {
-        name = 'className';
-      }
-      props[name] = attribute.value;
-    });
-
-    var args = [props].concat(
-      Array.prototype.map.call(el.childNodes, domToReact)
-    );
-
-    return DOM[funcName].apply(DOM, args);
-  }
-  else if (el.nodeType == 3) {
+  if (el.nodeType == 3) {
     return el.nodeValue;
   }
+ 
+  if (el.nodeType != 1) {
+    return "";
+  }
+  
+  var funcName = el.nodeName.toLowerCase();
+  var props = {};
+ 
+  Array.prototype.forEach.call(el.attributes, function(attribute) {
+    var name = attribute.nodeName;
+    if (name == 'class') {
+      name = 'className';
+    }
+    props[name] = attribute.value;
+  });
+ 
+  var args = [props].concat(
+    Array.prototype.map.call(el.childNodes, domToReact)
+  );
+ 
+  return DOM[funcName].apply(DOM, args);
 };
