4ae96f46f60ee20365d4039268b2899403622683
Tom Ashworth <tashworth@twitter.com>
2014-12-05 17:37:22 +0000

    Fix the proxy

--- server/tweetdeck-proxy.js
+++ server/tweetdeck-proxy.js
@@ -6,40 +6,41 @@ var TD = {
   port: 443
 };
 
-module.exports = http.createServer(function (req, res) {
+module.exports = http.createServer(function (request, response) {
   var data = {
-    method: req.method,
+    method: request.method,
     host: TD.host,
     port: TD.port,
-    path: req.url,
-    headers: req.headers
+    path: request.url,
+    headers: request.headers
   };
 
   delete data.headers.host;
 
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  response.setHeader('Access-Control-Allow-Origin', request.headers.origin);
   if (data.headers['access-control-request-headers']) {
-    res.setHeader('Access-Control-Allow-Headers', data.headers['access-control-request-headers']);
+    response.setHeader('Access-Control-Allow-Headers', data.headers['access-control-request-headers']);
   }
 
-  if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Max-Age', '86400');
-    res.writeHead(200);
-    res.end();
+  if (request.method === 'OPTIONS') {
+    response.setHeader('Access-Control-Max-Age', '86400');
+    response.writeHead(200);
+    response.end();
     return;
   }
 
-  var tweetdeckReq = https.request(data, function(twres) {
-    Object.keys(twres.headers).forEach(function (k) {
-      headers[k] = twres.headers[k];
-    });
-    res.writeHead(twres.statusCode, headers);
-    twres.pipe(res);
+  var tweetdeckReq = https.request(data, function(upstreamResponse) {
+    var headers = Object.keys(upstreamResponse.headers).reduce(function (headers, k) {
+      headers[k] = upstreamResponse.headers[k];
+      return headers;
+    }, {});
+    response.writeHead(upstreamResponse.statusCode, headers);
+    upstreamResponse.pipe(response);
   });
 
   tweetdeckReq.on('error', function(e) {
-    res.destroy();
+    response.destroy();
   });
 
-  req.pipe(tweetdeckReq);
+  request.pipe(tweetdeckReq);
 });
