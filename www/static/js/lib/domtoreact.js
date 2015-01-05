62c8a6249fd66701caa03135a439909dff158b04
Pascal Hartig <passy@twitter.com>
2015-01-05 16:01:57 +0000

    dom2react style

--- www/static/js/lib/domtoreact.js
+++ www/static/js/lib/domtoreact.js
@@ -1,28 +1,26 @@
+'use strict';
 var DOM = require('react').DOM;
- 
+
 module.exports = function domToReact(el) {
-  if (el.nodeType == 3) {
+  if (el.nodeType === 3) {
     return el.nodeValue;
   }
- 
-  if (el.nodeType != 1) {
-    return "";
+
+  if (el.nodeType !== 1) {
+    return '';
   }
-  
-  var funcName = el.nodeName.toLowerCase();
-  var props = {};
- 
+
+  const funcName = el.nodeName.toLowerCase();
+  const props = {};
+
   Array.prototype.forEach.call(el.attributes, function(attribute) {
-    var name = attribute.nodeName;
-    if (name == 'class') {
-      name = 'className';
-    }
+    const name = (name === 'class') ? 'className' : attribute.nodeName;
     props[name] = attribute.value;
   });
- 
-  var args = [props].concat(
+
+  const args = [props].concat(
     Array.prototype.map.call(el.childNodes, domToReact)
   );
- 
+
   return DOM[funcName].apply(DOM, args);
 };
