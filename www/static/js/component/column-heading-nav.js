fba12e0725d70d1683c410fcf47467b548cf9cb1
Pascal Hartig <passy@twitter.com>
2015-01-06 12:15:51 +0000

    Column headings refactor

--- www/static/js/component/column-heading-nav.js
+++ www/static/js/component/column-heading-nav.js
@@ -1,13 +1,12 @@
 'use strict';
 
 const React = require('react');
-const DOM = React.DOM;
 const MobileColumnHeadings = require('../lib/mobilecolumnheadings');
 
 const ColumnHeading = React.createClass({
   render() {
-    return DOM.li({ className: 'column-headings-nav-item' },
-      this.props.title
+    return (
+      <li className="column-headings-nav-item">{this.props.title}</li>
     );
   }
 });
@@ -18,24 +17,24 @@ module.exports = React.createClass({
       swiper: null
     };
   },
+
   componentDidMount() {
-    var mobileColumnHeadings = new MobileColumnHeadings(this.refs.headingsNav.getDOMNode());
+    const mobileColumnHeadings = new MobileColumnHeadings(this.refs.headingsNav.getDOMNode());
 
     this.props.swiper.on('render', (pos) => mobileColumnHeadings.render(pos));
-    this.props.swiper.on('layoutupdate', () => mobileColumnHeadings.updateLayout());
+    this.props.swiper.on('layoutupdate', _ => mobileColumnHeadings.updateLayout());
   },
+
   render() {
-    return DOM.div({ className: 'column-headings-nav', ref: 'headingsNav' },
-      DOM.ol({ className: 'column-headings-nav-panner' },
-        this.props.columns.map(ColumnHeading)
-      ),
-      DOM.div({ className: 'column-headings-nav-pips' },
-        this.props.columns.map(function(column) {
-          return DOM.div({ className: 'pip', key:column.key},
-            DOM.div({ className: 'pip-fill' })
-          );
-        })
-      )
+    return (
+      <div className="column-headings-nav" ref="headingsNav">
+        <ol className="column-headings-nav-panner">
+          {this.props.columns.map(c => <ColumnHeading title={c.title} />)}
+        </ol>
+        <div className="column-headings-nav-pips">
+          {this.props.columns.map(column => <div className="pip" key={column.key}><div className="pip-fill" /></div>)}
+        </div>
+      </div>
     );
   }
 });
