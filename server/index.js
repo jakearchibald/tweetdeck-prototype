b95948c5603c291455ffa4c399ec92d358fff4b7
Pascal Hartig <passy@twitter.com>
2015-01-07 11:07:15 +0000

    Server indentation

--- server/index.js
+++ server/index.js
@@ -3,38 +3,37 @@ var swig = require('swig');
 var path = require('path');
 
 module.exports.createApp = function (opts) {
+  var app = express();
 
-	var app = express();
+  app.engine('html', swig.renderFile);
+  app.set('view cache', false);
+  app.set('disable service worker', false);
+  swig.setDefaults({ cache: false });
 
-	app.engine('html', swig.renderFile);
-	app.set('view cache', false);
-	app.set('disable service worker', false);
-	swig.setDefaults({ cache: false });
+  // simple logger
+  app.use(function(req, res, next){
+    console.log('%s %s', req.method, req.url);
+    next();
+  });
 
-	// simple logger
-	app.use(function(req, res, next){
-	  console.log('%s %s', req.method, req.url);
-	  next();
-	});
+  app.use('/tweetdeck-prototype/static', express.static(__dirname + '/../www/static'));
 
-	app.use('/tweetdeck-prototype/static', express.static(__dirname + '/../www/static'));
+  app.get(RegExp('^/(tweetdeck-prototype)?$'), function(req, res) {
+    res.redirect('/tweetdeck-prototype/');
+  });
 
-	app.get(RegExp('^/(tweetdeck-prototype)?$'), function(req, res) {
-	  res.redirect('/tweetdeck-prototype/');
-	});
+  app.get('/tweetdeck-prototype/', function(req, res) {
+    res.render('../www/index.html');
+  });
 
-	app.get('/tweetdeck-prototype/', function(req, res) {
-	  res.render('../www/index.html');
-	});
+  var swPath = '../tweetdeck-prototype/www/static/build/js/sw.js';
+  if (opts.disableServiceWorker) {
+    swPath = '../tweetdeck-prototype/www/static/js/sw/sw-disabled.js';
+  }
 
-	var swPath = '../tweetdeck-prototype/www/static/build/js/sw.js';
-	if (opts.disableServiceWorker) {
-		swPath = '../tweetdeck-prototype/www/static/js/sw/sw-disabled.js';
-	}
+  app.get('/tweetdeck-prototype/service-worker.js', function(req, res) {
+    res.sendFile(path.resolve(swPath));
+  });
 
-	app.get('/tweetdeck-prototype/service-worker.js', function(req, res) {
-	  res.sendFile(path.resolve(swPath));
-	});
-
-	return app;
+  return app;
 }
