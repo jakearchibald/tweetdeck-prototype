8a5ad306809bc13ea1646a881f9bc4939968ec75
Pascal Hartig <passy@twitter.com>
2015-01-07 14:29:35 +0000

    ES6ify service worker fetch code

--- www/static/js/sw/index.js
+++ www/static/js/sw/index.js
@@ -13,11 +13,10 @@ self.addEventListener('install', function(event) {
   )
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response =>
+      response || fetch(event.request))
   );
 });
 
