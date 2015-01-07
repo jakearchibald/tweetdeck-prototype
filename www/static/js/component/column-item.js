7d5fb6706155fb8d9a93ca3aa3878580274705a2
Jake Archibald <jaffathecake@gmail.com>
2015-01-07 11:39:06 +0000

    Simplified tweet layout

--- www/static/js/component/column-item.js
+++ www/static/js/component/column-item.js
@@ -9,17 +9,17 @@ module.exports = React.createClass({
   render() {
     return (
       <article className="tweet media" key={this.props.item.id}>
-        <div className="tweet__avatar media__img">
-          <img src={this.props.item.user.avatarBigger} alt="" />
-        </div>
         <h1 className="tweet__title">
-          <span className="tweet__name">{this.props.item.user.name} </span>
-          <span className="tweet__username">@{this.props.item.user.screenName} </span>
-          <span className="tweet__time">{utils.relativeTime(this.props.item.date.getTime())}</span>
+          <img className="tweet__avatar" src={this.props.item.user.avatarBigger} alt="" />
+          <span className="tweet__title-text">
+            <span className="tweet__name">{this.props.item.user.name} </span>
+            <span className="tweet__username">@{this.props.item.user.screenName} </span>
+            <span className="tweet__time">{utils.relativeTime(this.props.item.date.getTime())}</span>
+          </span>
         </h1>
         <div className="tweet__user">
         </div>
-        <div className="media__body" dangerouslySetInnerHTML={{__html: this.props.item.getHTML()}} />
+        <div className="tweet__body" dangerouslySetInnerHTML={{__html: this.props.item.getHTML()}} />
       </article>
     );
   }
