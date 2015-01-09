6a771db0432f8af4a449e888df15c08f7100ca71
Pascal Hartig <passy@twitter.com>
2015-01-09 11:55:59 +0000

    Don't. Look. At. This. Code.
    So this retries favorites, and it's a bloddy fucking mess.
    
    Things that are awful:
    
    - There's no persistence of the retry queue, so as soon as the SW dies
      the state dies with it. Should be fairly easy to serialize this in
      IDB, though.
    - The 'fetch' handler is terrible. We should have a proper router-y kind
      of thing in there that dispatches to separate handlers.
    - The retry mechanism shouldn't (exclusively) back off globally, but on
      a per-request basis.
    - Persistent failures are going to stay around for forever. (This would
      be really easy to fix, though.)

--- www/static/js/sw/index.js
+++ www/static/js/sw/index.js
@@ -1,5 +1,6 @@
 const caches = require('../lib/serviceworker-cache-polyfill');
 const Promise = require('../lib/promise');
+const Heap = require('qheap');
 
 self.addEventListener('install', function(event) {
    event.waitUntil(
@@ -13,7 +14,77 @@ self.addEventListener('install', function(event) {
   )
 });
 
+class RetryObject {
+  constructor(request) {
+    this.request = request;
+    this.retries = 0;
+  }
+
+  inc() {
+    return new RetryObject(this.request.clone(), this.retries + 1);
+  }
+}
+
+class RetryQueue {
+  constructor() {
+    this.timeout = null;
+    this.resetDelay();
+    this.queue = new Heap({
+      compar: (a, b) => (a.retries <= b.retries ? -1 : 1)
+    });
+  }
+
+  resetDelay() {
+    this.delay = 200;
+  }
+
+  run() {
+    console.log('RetryQueue: run()', this);
+    if (this.queue.length === 0) {
+      this.abort();
+      return;
+    }
+
+    const next = this.queue.shift();
+    console.log('RetryQueue: run next=', next);
+    // Really stupidly naive implementation. Ignores retry count for now,
+    // doesn't work on a per-request basis but globally and only exponentially
+    // increases the retry delay without an upper bound.
+    const nextPlusOne = next.inc();
+    fetch(next.request).then(this.resetDelay.bind(this)).catch(e => {
+      console.error('SW Fetch error', e);
+      this.delay *= 2;
+      this.queue.push(nextPlusOne);
+    });
+    this.timeout = setTimeout(this.run.bind(this), this.delay);
+  }
+
+  abort() {
+    console.log('RetryQueue: abort()');
+    clearTimeout(this.timeout);
+    this.timeout = null;
+    this.resetDelay();
+  }
+
+  insert(request) {
+    this.queue.push(new RetryObject(request.clone()));
+    this.run();
+  }
+}
+const retryQueue = new RetryQueue();
+
+// General caching
 self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (url.pathname.startsWith('/oauth/proxy/twitter/https%3A%2F%2Fapi.twitter.com%2F1.1%2Ffavorites%2Fcreate.json')) {
+    console.log('sw', 'is fav', event.request.url);
+    const request = event.request.clone();
+    event.respondWith(
+      fetch(event.request).catch(retryQueue.insert.bind(retryQueue, request))
+    );
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then(response =>
       response || fetch(event.request))
